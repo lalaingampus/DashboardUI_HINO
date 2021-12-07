d3b7ef3475530daa4388e08bfc132ac10ec2db64
lalaingampus <bottlewar@gmail.com>
2021-12-07 17:43:02 +0700

    initial commit

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '86531',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '86531',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '86531',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '86531',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '86531',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
